9cde9b96ab7c8abedb6f46dd623825395231f4b2
Nishith2704 <nishith4702@gmail.com>
2024-05-23 23:37:27 +0530

    this is a new datatypes list

---
+++ 02_datatypes/index.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello this is caffinated code") here we are using node js not browser
+console.log(3
+    +3) //code read
+ console.log("nishi")
+ let name="nishith"
+ let age=19
+ let isLoggedIn=false
+ let state;
+ 
+
+ //number=>2 topwr 53
+ //(primitve datatype)
+// bigInt
+//string => single or double we prefer double
+//boolean (true or false)
+//null(stand alone value)
+//undefined(it's like a placeholder )
+//symbol(using react)to find the uniquess in figma we would use 
+
+
+//object
+console.log(typeof "nishith") // it is an string
+console.log(typeof 234234153123412341234) // it is an number
+console.log(typeof null)// it is an object (interview question)
+console.log(typeof undefined)// it is an object
