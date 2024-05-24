9b2b7e3e1c00af039f4857b5738c7ea52dd78414
Nishith2704 <nishith4702@gmail.com>
2024-05-24 23:48:35 +0530

    about the updation of the conversion in file

---
+++ comparison/index.js
@@ -0,0 +1,14 @@
+// console.log(2>1);// comparison in boolean value
+// problem in different datatype
+console.log("2">1)// not priditable value
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0) // here the (eqality check == and comparsion check (<,>,>=,<=) work differently)
+// comparsions convert null to number i:e treating to be 0
+// that is the reason why the null in 4l is false and null in 7L is true
+console.log(undefined>=0);
+
+// strict check (it also check the datatype)
+
+console.log("2"===2);
+
