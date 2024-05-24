8214bec6c788ac09ed3bcfb9a8d8feeb3fc2c2f5
Nishith2704 <nishith4702@gmail.com>
2024-05-24 23:11:05 +0530

    about string_to_number conversion

---
+++ 04_String_to_number_conversion/index.js
@@ -0,0 +1,38 @@
+let SomeNumber=33
+let StringNumber=String(SomeNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+let value =3
+let negValue=-value
+//console.log(negValue); it's -3
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(3**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="hello"
+let str2=" caffinated_bro"
+console.log( str1+str2)
+//complex discussion
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//it behaviour would 122 which is string .if first part is string then it would convert into the string
+// console.log(2+2+"3");//it behaviour would be 43
+// all the data is in the ECMA SCRIPT..............
+//BLUNDER PART
+    console.log(3+5+4%3);// it's not a good pratice ...
+console.log(+true);//its value is 1 .but (true+) is not possible
+console.log(+"");//its value is 0 .operated precedence
+//assignment operator
+let num1,num2,num3,num4
+num1=num2=num3=num4=2+2// here reability plays a vital role it's also a not good pratice
+
+gameounter=100;
+gameounter++;
+console.log(gameounter) // prefix and postfix see in mdn document
+
+
+
