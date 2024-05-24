14c6d1f38b6223e6f3e41de2f46e5238d4e865b5
Nishith2704 <nishith4702@gmail.com>
2024-05-25 00:28:06 +0530

    this is the summary of the datatype

---
+++ summary_datatypes/index.js
@@ -0,0 +1,31 @@
+// primitive
+
+// 7 types :String ,Numver, Boolean, null,undefined,Symbol,BigInt
+
+//non-primitve or referance type
+
+//Array , Objects , functions
+ 
+// const score=100
+
+//defining the symbol 
+const id=Symbol('123')
+const anotherid=Symbol('123') // 
+let a=100000n
+console.log(id===anotherid); // here the ReturnType is different in both case
+console.log(typeof(a)) // for declaration of the bigInt we use the "n" in the end of the integer
+
+const heroes =["gojo","luffy","zoro"];
+
+let myobj ={
+    name:"nishith",
+    age:22,
+}
+ 
+const myfuntion =function (){
+console.log("hello world");
+}
+console.log(myfuntion);
+console.log(typeof(myfuntion)) // for null the datatype would be Object (function is also called funtion object)
+console.log(typeof(myobj));  // object
+console.log(typeof(heroes));// object
