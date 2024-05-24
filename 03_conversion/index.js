0150a4b5777364debe3aa3084a186d06774dba92
Nishith2704 <nishith4702@gmail.com>
2024-05-24 14:50:14 +0530

    this is about the conversion

---
+++ 03_conversion/index.js
@@ -0,0 +1,23 @@
+let score = 33
+let score1="33abs"
+let bool=false;
+let bool1=true;
+let isLoggedIn=54
+let empty=""
+
+console.log(typeof score1);
+console.log(typeof (score1));
+
+let valueInInteger=Number(score1)
+console.log(valueInInteger);
+console.log(Number(bool));
+console.log(Boolean(isLoggedIn))
+console.log(empty);
+
+/* 1=>true;
+   0=>false;
+   "nishith"=>true;
+   */
+  let someNumber =33
+  let Stringnumber=String(someNumber)//it has become the string
+  console.log(Stringnumber);
