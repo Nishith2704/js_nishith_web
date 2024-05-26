00880bed03d217c47e890b6b9467e14cb465a802
Nishith2704 <nishith4702@gmail.com>
2024-05-26 23:02:07 +0530

    this is about more information about string *

---
+++ more_about_String/index.js
@@ -0,0 +1,31 @@
+// here the declartion of the string 
+const UserName="nishith" // here it is one type of declartion of the string
+const declaration_String=new String("Full_Stack_dev")
+console.log(declaration_String); // here the acutual declaration the string takes place
+console.log(UserName)// here it is the shorter version of acutal one
+
+console.log(UserName+" "+"profession is "+declaration_String); // it's older verison we see
+
+/* We could write*/
+console.log(`the developer ${UserName} is an ${declaration_String}`);
+// it is the modernized way of writing the string
+
+let str1="helloworld"
+console.log(str1);
+//here some the of the function types regarding to string
+const string=new String("this is caffinated")
+
+console.log(string.__proto__); // use of the proto use  but its syntax
+
+console.log(string.toUpperCase()); // here original value does not changes due to store at stack copy is proivided
+console.log(string.charAt(2));
+console.log(string.indexOf('t'));// why -1 in proto ... for normal it could be 0 o/p
+//big() not be used but manupulated using by css font size
+console.log(string.trim(2));
+
+
+
+
+
+
+
