64b27b216cf0007df3684c0156991618dfdd1b4c
Nishith2704 <nishith4702@gmail.com>
2024-06-06 17:10:32 +0530

    about the while loop , do while loop and alsa format of the code

---
+++ while/index.js
@@ -0,0 +1,41 @@
+ 
+
+// while (2==2){// enter the loop
+//     // loop which ever it could but it need to change for the terminated
+
+// }
+
+// let index = 0
+// while(index <= 10)  {
+//     console.log(`value of  index is ${index}`);  // here the part is to increment or decrement for the necceasary condition to occcur
+//     index+=2
+// }
+// for array
+//  let Myarray =["luffy","zoro","enel"]
+//  let arr=0
+//  while (arr < Myarray.length
+//  ) {
+//  console.log(`value is ${Myarray[arr]}`); 
+//  arr++;   // shorter the code is not best but it should be readble for everyone
+//  }
+
+ // do-while
+let score =1
+ do {
+    console.log(`score is ${score}`);
+    score ++;
+ } while (score <= 10);  
+
+
+let score1=11
+ do {
+    console.log(`score is ${score1}`);
+    score ++;
+ } while (score1 <= 10); 
+    // here it is the special case for which 
+    // the o/p is tell that the score is 11
+    // do is like work first then check the condition
+
+
+
+    // array loop common syntax which is in higher order
